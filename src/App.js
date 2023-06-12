4b1744e771a6f68d2fb83291b4f99ef171137c42
CHaNGo666 <rojo6666@yahoo.com.ar>
2023-06-12 15:28:58 -0300

    l c

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ axios.get("http://red666.somee.com/WeatherForecast",{ crossdomain: true })
 
   return (
   <>
-  <h1>Prueba conexion L 12-6 b</h1>
+  <h1>Prueba conexion L 12-6 c</h1>
   
   <div> Mas texto</div>
   </>
