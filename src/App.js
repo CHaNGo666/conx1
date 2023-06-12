b04b851092ba90449b1db172cbc7f14fd8e4abdb
CHaNGo666 <rojo6666@yahoo.com.ar>
2023-06-11 23:38:25 -0300

    dom e

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import './App.css';
 
-
+//  axios.get("http://red666.somee.com/WeatherForecast",{ crossdomain: true })
 function App() {
 
 
 
   useEffect(() => {
 
+    const config = {
+      headers:{
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTION",
+        "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, X-Requested-With"
+      }
+    };
 
-    axios.get("http://red666.somee.com/WeatherForecast",{ crossdomain: true }).then((result)=>{
+
+    axios.get("http://red666.somee.com/WeatherForecast",config)
+    .then((result)=>{
       console.log("resultado =======>>>>     ",result);
-    }).catch((error)=>{
+    })
+    .catch((error)=>{
       console.log("Error ===========>>>     ",error);
     });
 
@@ -21,7 +31,7 @@ function App() {
 
   return (
   <>
-  <h1>Prueba conexion Dom 11-6 e</h1>
+  <h1>Prueba conexion Dom 11-6 f</h1>
   
   <div> Mas texto</div>
   </>
